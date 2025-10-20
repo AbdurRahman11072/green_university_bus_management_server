f47db059df5d43515fb2a5affb282b9759b91217
AbdurRahman11072 <rjrahman014@gmail.com>
2025-10-20 13:12:40 +0600

    made improvement to busBookingZodSchema

--- src/app/modules/busBooking/busBooking.zodValidation.ts
+++ src/app/modules/busBooking/busBooking.zodValidation.ts
@@ -1,9 +1,13 @@
 import z from "zod";
 
 const busBookingZodSchema = z.object({
-  bookingSubject: z.string(),
+  bookingSubject: z
+    .string()
+    .min(10, "Subject must content 10 character")
+    .max(60, "Subject must have less then 60 character"),
   bookingDescription: z
     .string()
+    .min(10, "Description must content 10 character")
     .max(200, "Description must have less then 200 character"),
   bookingTime: z.string(),
   bookingDate: z.string(),
